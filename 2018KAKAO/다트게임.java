6a2e930df7536d705634c9122d157c1c34e07ecc
junsoo <01junsoo@gmail.com>
2020-07-31 17:33:02 +0900

    algorithm

--- 2018KAKAO/다트게임.java
+++ 2018KAKAO/다트게임.java
@@ -1,6 +1,6 @@
 /*
-	프로그래머스 - [카카오 인턴] 키패드 누르기
-	https://programmers.co.kr/learn/courses/30/lessons/67256
+	프로그래머스 - [카카오2018] 다트게임
+	https://programmers.co.kr/learn/courses/30/lessons/17682
 */
 
 #include <string>
